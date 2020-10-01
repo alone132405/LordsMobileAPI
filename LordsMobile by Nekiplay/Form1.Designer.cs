0f35bc36862d673bd77e66e0551e3bae54abdcf0
Nekiplay <Nekiplay@DESKTOP-HMTQFG5>
2020-10-01 12:23:50 +0500

    1

---
+++ LordsMobile by Nekiplay/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace LordsMobile_by_Nekiplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть карту";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "К замку";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Поиск ресурсов";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 116);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(298, 94);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Добыча ресов";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(89, 71);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(62, 17);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "Золото";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(18, 71);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(65, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Дерево";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(203, 19);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(82, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Распределить";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(124, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(73, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Добыть";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(18, 48);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(52, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "2 тир";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(174, 23);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(123, 23);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Прожать ручки";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(6, 69);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(84, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Обновить";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(156, 98);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Ресурсы";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Уровень: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Самоцветы: 0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Сила: 0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 216);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "LordsMobile API Example";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LordsMobile by Nekiplay/Form1.cs
@@ -0,0 +1,373 @@
+﻿using AForge.Imaging;
+using Binarysharp.MemoryManagement;
+using IronOcr;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace LordsMobile_by_Nekiplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process process = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+            MemorySharp sharp = new MemorySharp(process);
+            // Get the window
+            var window = sharp.Windows.MainWindow;
+            Utils utils = new Utils();
+            Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+            //Console.WriteLine(game.Width);
+            //Console.WriteLine(game.Height);
+            game.Save("game.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            Bitmap find = utils.ConvertImagePixelType(AForge.Imaging.Image.FromFile("game\\gomap.png"));
+            List<Rectangle> done = utils.Find(game, find);
+            foreach (Rectangle rec in done)
+            {
+                window.Activate();
+                window.Mouse.MoveTo(rec.X + 64, rec.Y + 32);
+                window.Mouse.ClickLeft();
+                Console.WriteLine(rec.X);
+                Console.WriteLine(rec.Y);
+
+            }
+            //done.Save("test.jpg");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process process = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+            MemorySharp sharp = new MemorySharp(process);
+            // Get the window
+            var window = sharp.Windows.MainWindow;
+            Utils utils = new Utils();
+            Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+            //Console.WriteLine(game.Width);
+            //Console.WriteLine(game.Height);
+            game.Save("game.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            Bitmap find = utils.ConvertImagePixelType(AForge.Imaging.Image.FromFile(@"game\\gotocastle.png"));
+            List<Rectangle> done = utils.Find(game, find);
+            if (done.Count == 1)
+            {
+                window.Activate();
+                window.Mouse.MoveTo(done[0].X, done[0].Y);
+                window.Mouse.ClickLeft();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true && checkBox2.Checked == true)
+            {
+                Process process = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+                MemorySharp sharp = new MemorySharp(process);
+                // Get the window
+                var window = sharp.Windows.MainWindow;
+                Utils utils = new Utils();
+                Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+                //Console.WriteLine(game.Width);
+                //Console.WriteLine(game.Height);
+                game.Save("game.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                Bitmap find = AForge.Imaging.Image.FromFile(@"game\\wood2.png");
+                find.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+                List<Rectangle> done = utils.Find(game, find, 0.5);
+                Console.WriteLine("Деревьев: " + done.Count);
+                if (done.Count >= 1)
+                {
+                    window.Activate();
+                    window.Mouse.MoveTo(done[0].X + 64, done[0].Y + 64);
+                    window.Mouse.ClickLeft();
+                }
+                else
+                {
+
+                }
+            }
+            if (checkBox1.Checked == true && checkBox3.Checked == true)
+            {
+                Process process = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+                MemorySharp sharp = new MemorySharp(process);
+                // Get the window
+                var window = sharp.Windows.MainWindow;
+                Utils utils = new Utils();
+                Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+                //Console.WriteLine(game.Width);
+                //Console.WriteLine(game.Height);
+                game.Save("game.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                Bitmap find = AForge.Imaging.Image.FromFile(@"game\\gold2.png");
+                find.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+                List<Rectangle> done = utils.Find(game, find, 0.5f);
+                if (done.Count >= 1)
+                {
+                    window.Activate();
+                    window.Mouse.MoveTo(done[0].X + 64, done[0].Y + 64);
+                    window.Mouse.ClickLeft();
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process process = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+            MemorySharp sharp = new MemorySharp(process);
+            // Get the window
+            var window = sharp.Windows.MainWindow;
+            Utils utils = new Utils();
+            Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+            //Console.WriteLine(game.Width);
+            //Console.WriteLine(game.Height);
+            game.Save("game.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            Bitmap find = AForge.Imaging.Image.FromFile(@"game\\gaterbutton.png");
+            find.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+            List<Rectangle> done = utils.Find(game, find, 0.6);
+            if (done.Count >= 1)
+            {
+                window.Activate();
+                window.Mouse.MoveTo(done[0].X + 32, done[0].Y + 32);
+                window.Mouse.ClickLeft();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (1 != 0)
+            {
+                Process process2 = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+                MemorySharp sharp = new MemorySharp(process2);
+                // Get the window
+                var window2 = sharp.Windows.MainWindow;
+                Utils utils2 = new Utils();
+                Bitmap game2 = utils2.ConvertImagePixelType(utils2.GetProgrammImage(process2));
+                //Console.WriteLine(game.Width);
+                //Console.WriteLine(game.Height);
+                game2.Save("game.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                Bitmap find2 = AForge.Imaging.Image.FromFile(@"game\\armyhave1.png");
+                find2.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+                List<Rectangle> done2 = utils2.Find(game2, find2, 0.6);
+                if (done2.Count >= 1)
+                {
+
+                }
+                else
+                {
+                    if (1 != 0)
+                    {
+                        Process process = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+                        // Get the window
+                        var window = sharp.Windows.MainWindow;
+                        Utils utils = new Utils();
+                        Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+                        //Console.WriteLine(game.Width);
+                        //Console.WriteLine(game.Height);
+                        game.Save("game.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                        Bitmap find = AForge.Imaging.Image.FromFile(@"game\\typevishkabutton.png");
+                        find.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+                        List<Rectangle> done = utils.Find(game, find, 0.6);
+                        if (done.Count >= 1)
+                        {
+                            window.Activate();
+                            window.Mouse.MoveTo(done[0].X + 32, done[0].Y + 32);
+                            window.Mouse.ClickLeft();
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    if (1 != 0)
+                    {
+                        Process process = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+                        // Get the window
+                        var window = sharp.Windows.MainWindow;
+                        Utils utils = new Utils();
+                        Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+                        //Console.WriteLine(game.Width);
+                        //Console.WriteLine(game.Height);
+                        game.Save("game.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                        Bitmap find = AForge.Imaging.Image.FromFile(@"game\\goattackbutton.png");
+                        find.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+                        List<Rectangle> done = utils.Find(game, find, 0.6);
+                        if (done.Count >= 1)
+                        {
+                            window.Activate();
+                            window.Mouse.MoveTo(done[0].X + 128, done[0].Y + 128);
+                            window.Mouse.ClickLeft();
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (1 != 0)
+            {
+                Process process = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+                MemorySharp sharp = new MemorySharp(process);
+                // Get the window
+                var window = sharp.Windows.MainWindow;
+                Utils utils = new Utils();
+                Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+                //Console.WriteLine(game.Width);
+                //Console.WriteLine(game.Height);
+                game.Save("game.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                Bitmap find = AForge.Imaging.Image.FromFile(@"game\\hand.png");
+                find.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+                List<Rectangle> done = utils.Find(game, find, 0.4);
+                if (done.Count >= 1)
+                {
+                    window.Activate();
+                    window.Mouse.MoveTo(done[0].X, done[0].Y);
+                    window.Mouse.ClickLeft();
+                }
+                else
+                {
+
+                }
+            }
+            Thread.Sleep(500);
+            if (1 != 0)
+            {
+                Process process = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+                MemorySharp sharp = new MemorySharp(process);
+                // Get the window
+                var window = sharp.Windows.MainWindow;
+                Utils utils = new Utils();
+                Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+                //Console.WriteLine(game.Width);
+                //Console.WriteLine(game.Height);
+                game.Save("game.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                Bitmap find = AForge.Imaging.Image.FromFile(@"game\\handall.png");
+                find.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+                List<Rectangle> done = utils.Find(game, find, 0.4);
+                if (done.Count >= 1)
+                {
+                    window.Activate();
+                    window.Mouse.MoveTo(done[0].X, done[0].Y);
+                    window.Mouse.ClickLeft();
+                }
+                else
+                {
+
+                }
+            }
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Process process = Process.GetProcessesByName("Lords Mobile").FirstOrDefault();
+            MemorySharp sharp = new MemorySharp(process);
+            var window = sharp.Windows.MainWindow;
+            Utils utils = new Utils();
+            if (1 != 0)
+            {
+                Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+                // Get the window
+                Bitmap find = AForge.Imaging.Image.FromFile(@"game\\power.png");
+                find.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+                List<Rectangle> done = utils.Find(game, find, 0.7);
+                string energy = "";
+                if (done.Count >= 1)
+                {
+                    //window.Mouse.MoveTo(done[0].X + 135, done[0].Y + 3);
+                    Bitmap crop = utils.Crop(game, new Rectangle(done[0].X + 135, done[0].Y + 3, 200, 30));
+                    crop.Save("Crop.png", System.Drawing.Imaging.ImageFormat.Png);
+                    string power = utils.GetTextFromImage(crop);
+                    label1.Text = "Сила: " + power;
+                }
+                else
+                {
+
+                }
+                sharp.Dispose();
+            }
+            if (1 != 0)
+            {
+                Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+                // Get the window
+                Bitmap find = AForge.Imaging.Image.FromFile(@"game\\power.png");
+                find.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+                List<Rectangle> done = utils.Find(game, find, 0.7);
+                if (done.Count >= 1)
+                {
+                    Bitmap crop = utils.Crop(game, new Rectangle(done[0].X + 155, done[0].Y + 55, 200, 30));
+                    crop.Save("Crop.png", System.Drawing.Imaging.ImageFormat.Png);
+                    string power = utils.GetTextFromImage(crop);
+                    label2.Text = "Самоцветы: " + power.Replace("Mt ", "");
+                }
+                else
+                {
+            
+                }
+                sharp.Dispose();
+            }
+            if (1 != 0)
+            {
+                Bitmap game = utils.ConvertImagePixelType(utils.GetProgrammImage(process));
+                // Get the window
+                Bitmap find = AForge.Imaging.Image.FromFile(@"game\\power.png");
+                find.Save("find.png", System.Drawing.Imaging.ImageFormat.Png);
+                List<Rectangle> done = utils.Find(game, find, 0.7);
+                if (done.Count >= 1)
+                {
+                    //window.Mouse.MoveTo(done[0].X + 10, done[0].Y + 156);
+                    Bitmap crop = utils.Crop(game, new Rectangle(done[0].X + 10, done[0].Y + 156, 33, 25));
+                    crop.Save("Crop.png", System.Drawing.Imaging.ImageFormat.Png);
+                    string power = utils.GetTextFromImage(crop);
+                    label3.Text = "Уровень: " + utils.TruncateLongString(power, 2, "");
+                }
+                else
+                {
+            
+                }
+                sharp.Dispose();
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
